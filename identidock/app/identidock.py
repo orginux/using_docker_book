c455f4430fe28d2bd5742b72bee4b07883021997
orginux <orginux@protonmail.com>
2019-10-27 19:47:13 +0300

    more monstres
    more monstres

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,23 +1,29 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
+import hashlib
 
 app = Flask(__name__)
+salt = "UNIQUE SALT"
 default_name = 'Emmett Li'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def mainpage():
-
     name = default_name
+    if request.method == 'POST':
+        name = request.form['name']
+
+    salted_name = salt + name
+    name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
 
     header = '<html><head><title>Identidock</title></head></html><body>'
     body = '''<form method="POST">
-        Hello <input type="text" name="name" value="{}">
+        Hello <input type="text" name="name" value="{0}">
         <input type="submit" value="submit">
         </form>
         <p>You look like a:
-        <img src="/monster/monster.png"/>
-        '''.format(name)
+        <img src="/monster/{1}"/>
+        '''.format(name, name_hash)
     footer = '</body></html>'
 
     return header + body + footer
@@ -26,7 +32,7 @@ def mainpage():
 @app.route('/monster/<name>')
 def get_identicon(name):
 
-    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=120')
     image = r.content
 
     return Response(image, mimetype='image/png')
